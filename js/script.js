3028e936e12ef46a8e9283a72776baea353cc82f
Tristan Young <tristan.young@verizon.com>
2019-06-24 17:58:28 -0500

    update

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@ $( document ).ready(function() {
     $('#name').focus();   
 });
 
-//if other option is selected show text field 
 $("#other-title").hide();
+//if other option is selected show text field 
+$('#title').change(function(e){
+    alert($(e.target).val());
+    if (select == 'Other'){
+        $("#other-title").show();
+    } else {
+        $("#other-title").hide();
+    }
+    
+});
