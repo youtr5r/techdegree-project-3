f07ecf5b06754097183df55c83d4b8e747bed6cf
Tristan Young <tristan.young@verizon.com>
2019-06-25 15:32:53 -0500

    update

--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@ $( document ).ready(function() {
     $('#name').focus();   
 });
 
-$("#other-title").hide();
-//if other option is selected show text field 
-$('#title').change(function(e){
-    alert($(e.target).val());
-    if (select == 'Other'){
+
+
+/*** 
+ Create text field using jQuery
+ place as the last element in field set by appendTo method
+***/
+$('<input/>')
+    .attr({ type: 'text', id: 'other-title', placeholder: 'Your Job Role'})
+    .appendTo('fieldset')
+    .hide();
+
+
+$('#title').on('click change', function(){
+    const jobTitle = $('#title option:selected').val();  
+    //alert(jobTitle);
+    if (jobTitle == 'other'){
+        
         $("#other-title").show();
+
     } else {
+
         $("#other-title").hide();
     }
-    
 });
