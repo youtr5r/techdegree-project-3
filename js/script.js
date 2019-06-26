68a825df47f42f4641fd54ea4e2212893cc9524b
Tristan Young <tristan.young@verizon.com>
2019-06-26 16:55:25 -0500

    update

--- js/script.js
+++ js/script.js
@@ -3,19 +3,9 @@ $( document ).ready(function() {
 });
 
 
-
+const color = $('')
 
 $('#other-title').hide();
-
-/*** 
- Create text field using jQuery
- place as the last element in field set by appendTo method
-***/
-// $('<input/>')
-//     .attr({ type: 'text', id: 'other-title', placeholder: 'Your Job Role'})
-//     .appendTo('fieldset')
-//     .hide();
-
 
 $('#title').on('click change', function(){
     const jobTitle = $('#title option:selected').val();  
@@ -30,6 +20,44 @@ $('#title').on('click change', function(){
     }
 });
 
+$('#design').find('option:first').hide();
+
+// $('#design > option:first').hide();
+
+$('#color option').prepend().text("Please Select a T-shirt Theme");
+$('#color option').hide();
 
 //tshirt design should match color
-//$('#color')
+$('#design').on('change',function (){
+    const selectTheme = $('#design option:selected').val();
+    const color = $('#color option:selected').val();
+    alert(color);
+    alert(selectTheme);
+    // const jsPuns = $('#design').val();
+    // const iHeartJs = $('#design').val();
+     
+    if(selectTheme == 'js puns') {
+        
+
+        color.eq(0).next().next().show();
+        color.eq(3).next().next().hide();
+
+
+    } else if(selectTheme == 'heart js') {
+        
+
+        color.eq(3).next().next().show();
+        color.eq(0).next().next().hide();
+    }
+ });
+
+
+//$('#color').prepend({})
+
+// $('#color').on('change',function (){
+// // const punsShirts = $("#color option:selected").text(); 
+// // alert(punsShirts);
+// // const shirt = $('option[textf$="(JS Puns shirt only)"');
+// // alert(shirt);
+// })
+
