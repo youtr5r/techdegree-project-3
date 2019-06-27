0accc3ebea6b42f1d2d933e725e80d675cd62941
Tristan Young <tristan.young@verizon.com>
2019-06-27 13:39:32 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
-    $('#name').focus();   
+    $('#name').focus();  
+    
 });
 
 
@@ -24,33 +25,65 @@ $('#design').find('option:first').hide();
 
 // $('#design > option:first').hide();
 
-$('#color option').prepend().text("Please Select a T-shirt Theme");
+$('#color option:selected').prepend().text("Please Select a T-shirt Theme");
 $('#color option').hide();
+
+// const jsPuns = $('#color option:contains(" + JS Puns + ")').val();
+//     const jsShirts = $('#color option:contains(" + JS shirt + ")').val(); 
 
 //tshirt design should match color
 $('#design').on('change',function (){
     const selectTheme = $('#design option:selected').val();
     const color = $('#color option:selected').val();
-    alert(color);
-    alert(selectTheme);
+    // const jsPuns = $('#color:contains(Puns)')
+    // const jsShirts = $('#color:contains('JS shirt')').attr('selected', 'true');
+    
     // const jsPuns = $('#design').val();
     // const iHeartJs = $('#design').val();
      
     if(selectTheme == 'js puns') {
         
-
-        color.eq(0).next().next().show();
-        color.eq(3).next().next().hide();
+        $('#color option:contains(Puns)').show();
+        $('#color option:contains(JS shirt)').hide();
+      
 
 
     } else if(selectTheme == 'heart js') {
+            
         
+        $('#color option:contains(JS shirt)').show();
+        $('#color option:contains(Puns)').hide();
 
-        color.eq(3).next().next().show();
-        color.eq(0).next().next().hide();
+
+        
+    } else {
+        $('#color option').hide();
     }
  });
 
+//  $('#design').on('change',function (){
+//     const selectTheme = $('#design option:selected').val();
+//     const color = $('#color option:selected').val();
+//     const jsPuns = $('#color option:contains(JS Puns)').text();
+//     const jsShirts = $('#color option:contains(JS shirt)').text();
+    
+//     // const jsPuns = $('#design').val();
+//     // const iHeartJs = $('#design').val();
+     
+//     if(selectTheme == 'js puns') {
+        
+
+//         $('#color option').eq(0).next().next().show(jsPuns);
+//         $('#color option').eq(3).next().next().hide(jsShirts);
+
+
+//     } else if(selectTheme == 'heart js') {
+        
+
+//         $('#color option').eq(3).next().next().show(jsShirts);
+//         $('#color option').eq(0).next().next().hide(jsPuns);
+//     }
+//  });
 
 //$('#color').prepend({})
 
